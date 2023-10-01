096a87730a3d75ae90b574785d4f1e27ea17c002
Ronner <ronner.navas@gmail.com>
2023-10-01 09:51:10 -0300

    Homerwork 08

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,15 +5,14 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
-  return nombre.chartAt().toUpperCase() + nombre.slice(1);
+  return nombre.charAt().toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-
-
+cb ()
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -30,11 +29,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-var sumar = 0
-arrayOfNumbers.reduce(function(prev, next){
-   prev + next
-})
-cb(sumar) 
+   var suma = 0;
+   arrayOfNumbers.forEach(function(elem){
+      suma = suma + elem
+   })
+  cb (suma) ;
 }
 
 function forEach(array, cb) {
@@ -42,6 +41,10 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+
+   array.forEach(function(elem){
+      cb(elem)
+   })
 }
 
 function map(array, cb) {
@@ -49,12 +52,20 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   var newArray =  array.map(function(elem){
+     return cb(elem)
+   }) 
+   return newArray;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var newArray = arrayOfStrings.filter(function(elem){
+      return elem.charAt() === "a"
+   });
+   return newArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
